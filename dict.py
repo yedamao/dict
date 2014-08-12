ea9b4f00b8bdac815d84d1254390c4ab3e8a607d
dave <logindavedye@gmail.com>
2014-08-12 15:22:47 +0800

    add countword, motion, use linux share wordlist

--- dict.py
+++ dict.py
@@ -1,18 +1,23 @@
 #!/usr/bin/env python3
 
 import sys
+import os
 
 
 def checkWordList(word):
+    """
+    check the word . if it in wordList .program continue
+    """
     flag = None
-    for line in open('/home/dave/dict/wordlist'):
+    for line in open('/usr/share/dict/american-english'):
         if line.strip() == word:
             flag = True
     if flag:
         return word
-    else:
-        print('false {}'.format(line.strip()))
-        print("Are you sure {:-^8} is a word?".format(word))
+    else:  # else the program exit
+        print()
+        print("(~_~): Are you sure {:-^8} is a word?".format(word))
+        print()
         sys.exit()
 
 
@@ -23,11 +28,11 @@ def searchWord(word):
         if line.split(':')[0] == word:
             text = line
             flag = True
-    if flag:
+    if flag:  # if find the word record print it.
         for _ in text.split(':'):
             print(_)
-    else:
-        print('{:-^8} not in list'.format(word))
+    else:     # else interactive ask user wheather add the word
+        print('(#_#): {:-^8} not in list'.format(word))
         print(' ')
         yes = {'y', 'yes'}
         if input('add {}. y/n: '.format(word)) in yes:
@@ -35,18 +40,35 @@ def searchWord(word):
 
 
 def addWord(word):
-    list = ['英音', '美音', 'vt.','vi.', 'adj.', 'adv.', 'n.', 'other. ']
+    """
+    add the user input
+    """
+    list = ['英音', '美音', 'vt.', 'vi.', 'adj.', 'adv.', 'n.', 'other. ']
     print(''+word)
-    content = str() 
+    content = str()
     for _ in list:
-        text = input(_ )
+        text = input(_)
         if text:
             content += ':' + _ + text
     line = word + content
     fout = open('/home/dave/dict/dict', 'a', encoding='utf8')
     fout.write(line+'\n')
     fout.close()
+    os.system('sort /home/dave/dict/dict > /home/dave/dict/dict.tmp')
+    os.system('mv /home/dave/dict/dict.tmp /home/dave/dict/dict')
     print('Add successfully!')
+
+
+def countWords():
+    """
+    traversal
+    """
+    nu = 0
+    for line in open('/home/dave/dict/dict'):
+        nu += 1
+    print()
+    print('(^_^): already have {} words!'.format(nu))
+    print()
 
 
 def main():
@@ -56,9 +78,11 @@ def main():
 
 if __name__ == "__main__":
     if len(sys.argv) == 1:
-        print('Usage:{} word1 word2 ...'.format(sys.argv[0]))
+        print('(*_*)Usage:{} [--options] [word1] [word2] ...'
+              .format(sys.argv[0]))
         sys.exit()
+    elif sys.argv[1] == '--count':
+        countWords()
     else:
         words = sys.argv[1:]
-
-main()
+        main()
