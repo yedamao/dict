a873ffd5ec198736e421a4e95a025579e6d3cef4
dave <logindaveye@gmail.com>
2014-10-22 21:32:38 +0800

    modify dict.go

--- dict.go
+++ dict.go
@@ -1,67 +1,71 @@
 package main
 
 import (
-    "fmt"
-    "os"
-    "log"
-    "strings"
-    "bufio"
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
 )
 
 func checkError(err error) {
-    if err != nil {
-        log.Fatal(err)
-        os.Exit(1)
-    }
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(1)
+	}
 }
 
 func readDictionary() []byte {
-    //open dictionary file
-    file , err := os.Open("dictionary")
-    checkError(err)
-    defer file.Close()
+	//open dictionary file
+	file, err := os.Open("dictionary")
+	checkError(err)
+	defer file.Close()
 
-    //get the file size
-    stat, err := file.Stat()
-    checkError(err)
+	//get the file size
+	stat, err := file.Stat()
+	checkError(err)
 
-    //read the file
-    data := make([]byte, stat.Size())
-    _, err = file.Read(data)
-    checkError(err)
+	//read the file
+	data := make([]byte, stat.Size())
+	_, err = file.Read(data)
+	checkError(err)
 
-    return data
+	return data
 }
 
 func searchWordLine(word string) string {
-    data := string(readDictionary())
-    wordLines := strings.Split(data, "\n")
-    var wordLine string
-    for i := 0; i < len(wordLines); i++ {
-        target := strings.Split(wordLines[i], ":")[0]
-        if strings.ToLower(word) == strings.ToLower(target) {
-            wordLine = wordLines[i]
-        }
-    }
-    return wordLine
+	data := string(readDictionary())
+	wordLines := strings.Split(data, "\n")
+	var wordLine string
+	for i := 0; i < len(wordLines); i++ {
+		target := strings.Split(wordLines[i], ":")[0]
+		if strings.ToLower(word) == strings.ToLower(target) {
+			wordLine = wordLines[i]
+		}
+	}
+	return wordLine
 }
 
 func printWordLine(wordLine string) {
-    for i := 0; i < len(strings.Split(wordLine, ":")); i++ {
-        fmt.Println(strings.Split(wordLine, ":")[i])
-    }
-    fmt.Println("_______________________________")
+	for i := 0; i < len(strings.Split(wordLine, ":")); i++ {
+		fmt.Println(strings.Split(wordLine, ":")[i])
+	}
+	fmt.Println("_______________________________")
 }
 
 func main() {
-    reader := bufio.NewReader(os.Stdin)
+	reader := bufio.NewReader(os.Stdin)
 
-    fmt.Print("Open Source Dictionary\n")
-    for {
-        fmt.Print("(OSD1.1):>")
-        line, _ := reader.ReadString('\n')
-        word := strings.Split(line, "\n")[0]
-        wordLine := searchWordLine(word)
-        printWordLine(wordLine)
-    }
+	fmt.Print("Open Source Dictionary\n")
+	for {
+		fmt.Print("(OSD1.1):>")
+		line, _ := reader.ReadString('\n')
+		word := strings.Split(line, "\n")[0]
+		wordLine := searchWordLine(word)
+        if wordLine != "" {
+            printWordLine(wordLine)
+        } else {
+            fmt.Println("Oooooooo!")
+        }
+	}
 }
